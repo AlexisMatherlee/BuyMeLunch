75c132da699d4c47201d800d39711de223826096
Alexis Matherlee <alexismatherlee@gmail.com>
2014-07-19 14:54:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuyMeLunch::Application.config.secret_key_base = 'ec13689ef7d5a69d98cfa9cda24c6d23e75acdad925cbfa17a4b66b16457df160cd60d37962ca1171d38200fbb639f9f09c50f2294a3209a3f10a588670302aa'
