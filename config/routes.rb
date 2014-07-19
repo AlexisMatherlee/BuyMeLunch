db7b1cd4b77b973e771137a90853a33510be7137
Alexis Matherlee <alexismatherlee@gmail.com>
2014-07-19 16:01:49 -0700

    getting started

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 BuyMeLunch::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :articles
+  
+  root :to => 'pages#index'
+
+    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
